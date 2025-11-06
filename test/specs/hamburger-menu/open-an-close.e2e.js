c86db781254bce640a669a6f63ebb3fd407f0de4
zbrown001 <zbrown@nanolos.com>
2025-11-06 09:56:18 -0700

    Refactor hamburger menu open/close tests
    Simplified and updated the hamburger menu E2E tests to use the latest menuComponent and loginPage objects. Added separate tests for closing the menu via button and ESC key, and removed unused imports and commented code.

--- test/specs/hamburger-menu/open-an-close.e2e.js
+++ test/specs/hamburger-menu/open-an-close.e2e.js
@@ -1,28 +1,18 @@
-// import { expect } from '@wdio/globals'
-// import LoginPage from '../pageobjects/login.page.js'
-// import SecurePage from '../pageobjects/secure.page.js'
+import menuComponent from "../../componentobjects/menu.component";
+import loginPage from "../../pageobjects/login.page";
 
-import menuComponent from "../../componentobjects/menu.component"
-import inventoryPage from "../../pageobjects/inventory.page"
-import loginPage from "../../pageobjects/login.page"
-import { $, browser } from '@wdio/globals'
+describe("Open and Close the Hamburger Menu", () => {
+  beforeEach(async () => {
+    await loginPage.login();
+  });
 
-// describe('My Login application', () => {
-//     it('should login with valid credentials', async () => {
-//         await LoginPage.open()
+  it("should open and close using the button", async () => {
+    await menuComponent.openMenu();
+    await menuComponent.closeMenu();
+  });
 
-//         await LoginPage.login('tomsmith', 'SuperSecretPassword!')
-//         await expect(SecurePage.flashAlert).toBeExisting()
-//         await expect(SecurePage.flashAlert).toHaveText(
-//             expect.stringContaining('You logged into a secure area!'))
-//     })
-// })
-
-describe('Open and Close the Hamburger Menu', () => {
-    it('should open and close as expected', async () => {
-        await loginPage.login()
-        await inventoryPage.openMenu()
-        await menuComponent.verifyIsOpen()
-        await browser.pause(5000)
-    })
-})
+  it("should open and close using the ESC key", async () => {
+    await menuComponent.openMenu();
+    await menuComponent.closeMenu(true);
+  });
+});
