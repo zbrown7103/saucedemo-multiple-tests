3ff1fb687a2a271a0125c7634a40cc49719bd965
zbrown001 <zbrown@nanolos.com>
2025-11-06 09:56:47 -0700

    Refactor login.page.js for consistency and clarity
    Updated code style to use consistent double quotes and semicolons. Changed the open() method to navigate to the login page instead of the inventory page. Improved method and property formatting for better readability.

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,32 +1,28 @@
-import { $, browser } from '@wdio/globals'
-import inventoryPage from './inventory.page'
+import { $, browser } from "@wdio/globals";
+import inventoryPage from "./inventory.page";
 
 class LoginPage {
-    // ---------- ELEMENT GETTTERS ---------- //
-    get inputUsername() {
-        return $('#user-name')
-    }
+  get inputUsername() {
+    return $("#user-name");
+  }
+  get inputPassword() {
+    return $("#password");
+  }
+  get btnSubmit() {
+    return $('input[type="submit"]');
+  }
 
-    get inputPassword() {
-        return $('#password')
-    }
+  open() {
+    return browser.url("https://www.saucedemo.com/");
+  }
 
-    get btnSubmit() {
-        return $('input[type="submit"]')
-    }
-
-    // ---------- METHODS ---------- //
-    open() {
-        return browser.url('https://www.saucedemo.com/inventory.html')
-    }
-
-    async login(username = 'standard_user', password = 'secret_sauce') {
-        this.open()
-        await this.inputUsername.setValue(username)
-        await this.inputPassword.setValue(password)
-        await this.btnSubmit.click()
-        await expect(inventoryPage.appLogo).toBeExisting()
-    }
+  async login(username = "standard_user", password = "secret_sauce") {
+    await this.open();
+    await this.inputUsername.setValue(username);
+    await this.inputPassword.setValue(password);
+    await this.btnSubmit.click();
+    await expect(inventoryPage.appLogo).toBeExisting();
+  }
 }
 
 export default new LoginPage();
